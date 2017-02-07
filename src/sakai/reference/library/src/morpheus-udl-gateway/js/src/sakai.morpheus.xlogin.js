eae117276995ad63ad6ed6613d0b19d9dada7205
Alex Balleste <alex.balleste@gmail.com>
2017-02-07 08:39:05 +0100

    MSONZE-20 Afegida la primera versió del skin de la UdL

---
+++ src/sakai/reference/library/src/morpheus-udl-gateway/js/src/sakai.morpheus.xlogin.js
@@ -0,0 +1,42 @@
+
+$PBJQ(document).ready(function(){
+
+  $PBJQ("details", "#footer").attr('open', '');
+
+  $PBJQ('.Mrphs-portalWrapper').css('min-height', ( $PBJQ(window).outerHeight(true) - 95 ) + 'px' );
+
+  if( $PBJQ('#loginLink2').length == 1 ){
+
+    $PBJQ('#loginLink2').click( function( e ){
+
+      $PBJQ('body').append('<div id="Mrphs-xlogin-container" />');
+      $PBJQ('#Mrphs-xlogin-container').load('/portal/xlogin #Mrphs-xlogin',function(){
+        $PBJQ('#Mrphs-xlogin-container').addClass('loaded');
+        $PBJQ('#Mrphs-xlogin').addClass('loadedByAjax');
+        $PBJQ('#eid').focus();
+      });
+      $PBJQ('.Mrphs-portalWrapper').addClass('blurry');
+
+      $PBJQ('body').append('<div id="loginPortalMask" />');
+      $PBJQ('#loginPortalMask').bgiframe();
+      
+      $PBJQ('#loginPortalMask').click(function(){
+        $PBJQ('#loginPortalMask').remove();
+        $PBJQ('#Mrphs-xlogin-container').remove();
+        $PBJQ('.Mrphs-portalWrapper').removeClass('blurry');
+      });
+
+      e.preventDefault();
+
+    });
+  }
+
+});
+
+$(window).scroll(function(){
+  if( $(document).scrollTop() > 144 ){
+    $('#toolMenu').addClass('scrolled');
+  } else {
+    $('#toolMenu').removeClass('scrolled');
+  }
+});
---
+++ src/sakai/reference/library/src/morpheus-udl/js/src/sakai.morpheus.umu.js
@@ -0,0 +1,7 @@
+/**
+* For the UMU Skin
+*/
+
+$PBJQ(document).ready(function(){
+	$PBJQ("details", "#footer").attr('open', '');
+});
