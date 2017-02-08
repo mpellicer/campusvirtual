14f806e518c8c80a526b92295bc1ac391f6222c8
Alex Balleste <alex.balleste@gmail.com>
2017-02-08 10:00:51 +0100

    MSONZE-43 Added custom site's name cut method

--- src/sakai/kernel/kernel-impl/src/main/java/org/sakaiproject/util/impl/FormattedTextImpl.java
+++ src/sakai/kernel/kernel-impl/src/main/java/org/sakaiproject/util/impl/FormattedTextImpl.java
@@ -1292,9 +1292,9 @@ public class FormattedTextImpl implements FormattedText
             if (siteTitleCutMethod.length==2) {
                 cutMethod[0] = Integer.parseInt(siteTitleCutMethod[0]);
                 cutMethod[1] = Integer.parseInt(siteTitleCutMethod[1]);
-                if (cutMethod[0]+cutMethod[1]!=100) {
+                /*if (cutMethod[0]+cutMethod[1]!=100) {
                     throw new Exception("Invalid cut method values: "+cutMethod[0]+" + "+cutMethod[1]+" != 100");
-                }
+                }*/
             }
         } catch (Throwable ex) {
             cutMethod[0] = 100; cutMethod[1] = 0;
@@ -1320,17 +1320,41 @@ public class FormattedTextImpl implements FormattedText
             result = result.trim();
             if ( result.length() > siteTitleMaxLength && siteTitleMaxLength >= 10 ) {
                 int[] siteTitleCutMethod = getCutMethod(cutMethod);
-                int begin = Math.round(((siteTitleMaxLength-cutSeparator.length())*siteTitleCutMethod[0])/100);
+                /*int begin = Math.round(((siteTitleMaxLength-cutSeparator.length())*siteTitleCutMethod[0])/100);
                 int end = Math.round(((siteTitleMaxLength-cutSeparator.length())*siteTitleCutMethod[1])/100);
                 // Adjust odd character to the begin
                 begin += (siteTitleMaxLength - (begin + cutSeparator.length() + end));
-                result = ((begin>0)?result.substring(0,begin):"") + cutSeparator +((end>0)?result.substring(result.length()-end):"");
+                result = ((begin>0)?result.substring(0,begin):"") + cutSeparator +((end>0)?result.substring(result.length()-end):"");*/
+                int cutTail = siteTitleCutMethod[1];
+                int cutPoint = result.length() - cutTail;
+                result = ((cutPoint>0) ? result.substring(0,siteTitleMaxLength - (cutTail + cutSeparator.length())) : "") + cutSeparator +((cutTail>0) ? result.substring(result.length()-cutTail): "");
+
             } else if ( result.length() > siteTitleMaxLength ) {
                 result = result.substring(0,siteTitleMaxLength);
             }
         }
         return result;
     }
+    
+	protected String getUdLResumeTitle(String fullTitle,int cutTail,int siteTitleMaxLength,String cutSeparator) {
+		String titleStr = fullTitle;
+		
+		if ( titleStr != null )
+		{
+			titleStr = titleStr.trim();
+			if ( titleStr.length() > siteTitleMaxLength && siteTitleMaxLength >= 10 ) 
+			{
+				
+			} 
+			else if ( titleStr.length() > siteTitleMaxLength ) 
+			{
+				titleStr = titleStr.substring(0,siteTitleMaxLength);
+			}
+			//titleStr = titleStr.trim();
+		}
+		
+		return titleStr;
+	}
 
 
     // PRIVATE STUFF
